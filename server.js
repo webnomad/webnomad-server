17027ba7dab45381d0e35cb9fd0824d51a78b87b
Vincent Agnano <vincent.agnano@scopyleft.fr>
2016-01-14 23:05:04 +0100

    added automatic address generation

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 if (Meteor.isServer) {
-  Spots = new Mongo.Collection("spots");
+  Spots = new Mongo.Collection('spots');
 
-  Meteor.publish('SpotsAll', () => {
-    var spots = Spots.find({});
-    return spots;
+  Spots.before.insert((userId, doc) => {
+    var url = `http://nominatim.openstreetmap.org/reverse?format=json&lat=${doc.position.latitude}&lon=${doc.position.longitude}`;
+    var response = HTTP.call('GET', url);
+    doc.address = response.data.address;
   });
 }
